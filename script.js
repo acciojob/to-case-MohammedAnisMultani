03a291eb437aacaa4866fcedb6c20bb39115ee80
MohammedAnisMultani <147980347+MohammedAnisMultani@users.noreply.github.com>
2024-02-22 16:08:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 function toCase(text) {
-  // write your code here
+ let ans = text.trim()
+let string = ""
+for(let i=0; i<text.length; i++){
+  if(text.charAt(i) != " "){
+    string += text.charAt(i);
+  }
+}
+let str = ""
+str = str + string.toLowerCase()
+str = str + "-";
+str= str + string.toUpperCase()
+
+return str
 }
 
 // DO not change the code below
 
-const text = prompt("Enter text:");
-alert(toCase(text));
+const text = prompt("Enter text:"); 
+alert(toCase(text));  
